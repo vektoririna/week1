8e1ce2217cb0e16ca21e7491c73650d6a94a1ad6
vektoririna <60616025+vektoririna@users.noreply.github.com>
2021-05-12 17:22:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,78 @@
-export default (express, bodyParser, fs, crypto, http) => {
-    const app = express();
+import pug from 'pug';
 
-    var urlencodedParser = bodyParser.urlencoded({ extended: false })
+export default (express, bodyParser, createReadStream, writeFileSync, crypto, http, m, User, puppeteer) => {
+    const app = express();
+    const urlencodedParser = bodyParser.urlencoded({ extended: false });
+    const json_parser = bodyParser.json();
+    const url = 'mongodb+srv://danilabukin:danilabukin>@cluster0.wcetp.mongodb.net/week7demo?retryWrites=true&w=majority'
 
     app
+    .use((r, res, next) => r.res.set({
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,POST,PUT,PATCH,OPTIONS,DELETE"}) && next())
+    .use(bodyParser.urlencoded({ extended: true }))
+    .all('/req/', (req, res) => {
+        const addr = req.method === 'POST' ? req.body.addr : req.query.addr;
 
-    .use((req, res, next) => {
-      res.append('Access-Control-Allow-Origin', ['*']);
-      res.append('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,OPTIONS,DELETE');
-      next();
-  })
-
-    .use(bodyParser.json())
-
-    .all('/login/', (req, res) => {
-        res.end("sorokina_irina");
+        http.get(addr, (r, b = '') => {
+            r
+            .on('data', d => b += d)
+            .on('end', () => res.send(b));
+        });
     })
+    .get('/login/', (req, res) => res.send('bee_joo'))
+    .get('/code/', (req, res) => fs.createReadStream(import.meta.url.substring(7)).pipe(res))
+    .get('/sha1/:input/', (req, res) => res.send(crypto.createHash('sha1').update(req.params.input).digest('hex')))
+    .post('/insert/', urlencodedParser, async (req, res) => {
+        const log = req.body.login;
+        const pass = req.body.password;
+        const url = req.body.URL;
 
-    .all('/code/', (req, res) => {
-        fs.createReadStream("./app.js").pipe(res);
-    })
+        await m.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    .all('/sha1/:input', (req, res) => {
-        const hash = crypto.createHmac('sha1', req.params['input'])
-        .digest('hex');
-        res.end(String(hash))
+        const newUser = new User({login: log, password: pass});
+        try {
+            await newUser.save();
+            res.status(201);
+        } catch (e) {
+            res.status(400);
+        }
+        res.end();
     })
+    .get('/test/', urlencodedParser, async (req, res) => {
 
-    .get('/req/', (req, res) => {
-        const url = req.query.addr;
-        http.get(String(url), response => {
-            let data = '';
-            response.on('data', chunk => {
-                data += chunk;
-            })
-            response.on('end', () => {
-                res.end(data);
-            })
-        })
-    })
+        const browser = await puppeteer.launch({args: ['--no-sandbox']});
+        const page = await browser.newPage();
+        await page.goto(req.query.URL);
+        await page.click('#bt');
+        const input = await page.$('#inp');
+        let value = await page.evaluate(inp => inp.value, input);
+        res.send(value);
 
-    .post('/req/', urlencodedParser, (req, res) => {
-        const url = req.body.addr;
-        http.get(String(url), response => {
-            let data = '';
-            response.on('data', chunk => {
-                data += chunk;
-            })
-            response.on('end', () => {
-                res.end(data);
-            })
-        })
+        res.send(url);
+        
     })
-
-    .all('/*', (req, res) => {
-        res.end("sorokina_irina");
+    .get('/wordpress/*', (req, res) => {
+        console.log('http://f0541150.xsph.ru/wordpress/'+req.params[0]);
+        res.header('Content-Type', 'application/json');
+        void http.get('http://f0541150.xsph.ru/wordpress/'+req.params[0], (r, buffer='') => {
+            r
+            .on('data', data => buffer += data)
+            .on('end', () => res.send(buffer));
+        });
     })
+    .post('/render/', json_parser, urlencodedParser, (req, res) => {
+      let addr = req.query.addr;
+      console.log(addr, req.body.random2, req.body.random3);
+      void http.get(addr, (r, buffer='') => {
+            r
+            .on('data', data => buffer += data)
+            .on('end', () => writeFileSync('views/data.pug', buffer));
+        });
+        res.render('data.pug', {'random2': req.body.random2, 'random3': req.body.random3});
+    })
+    
+    .all('/*', r => r.res.send('bee_joo'));
 
     return app;
 }
